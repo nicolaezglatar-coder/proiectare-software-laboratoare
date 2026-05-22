9b03ba55fb288fe619b7d07ad713ea16afb82f95
nicolae zglatar <nicolae.zglatar@ulbsibiu.ro>
2026-05-22 03:58:31 +0300

    L8: 8.5.2 este implementata

--- src/main/java/ro/ulbs/proiectaresoftware/lab8/ApplicationLab8.java
+++ src/main/java/ro/ulbs/proiectaresoftware/lab8/ApplicationLab8.java
@@ -1,6 +1,7 @@
 package ro.ulbs.proiectaresoftware.lab8;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -13,8 +14,12 @@ import java.io.IOException;
 public class ApplicationLab8 {
     public static void main(String[] args) {
         String inputFile = "laborator8_input.xlsx";
-        String outputFile = "laborator8_output.xlsx";
 
+        citesteSiGenereazaOutput1(inputFile, "laborator8_output.xlsx");
+        genereazaOutput2(inputFile, "laborator8_output2.xlsx");
+    }
+
+    public static void citesteSiGenereazaOutput1(String inputFile, String outputFile) {
         try (
                 FileInputStream inputStream = new FileInputStream(inputFile);
                 Workbook workbook = new XSSFWorkbook(inputStream)
@@ -25,23 +30,8 @@ public class ApplicationLab8 {
 
             for (Row row : sheet) {
                 for (Cell cell : row) {
-                    switch (cell.getCellType()) {
-                        case STRING:
-                            System.out.print(cell.getStringCellValue() + "\t");
-                            break;
-
-                        case NUMERIC:
-                            System.out.print(cell.getNumericCellValue() + "\t");
-                            break;
-
-                        case BOOLEAN:
-                            System.out.print(cell.getBooleanCellValue() + "\t");
-                            break;
-
-                        default:
-                            System.out.print(" \t");
-                            break;
-                    }
+                    afiseazaCelula(cell);
+                    System.out.print("\t");
                 }
                 System.out.println();
             }
@@ -61,6 +51,122 @@ public class ApplicationLab8 {
         } catch (IOException e) {
             System.out.println("Eroare la citirea/scrierea fisierului Excel.");
             e.printStackTrace();
+        }
+    }
+
+    public static void genereazaOutput2(String inputFile, String outputFile) {
+        try (
+                FileInputStream inputStream = new FileInputStream(inputFile);
+                Workbook inputWorkbook = new XSSFWorkbook(inputStream);
+                Workbook outputWorkbook = new XSSFWorkbook()
+        ) {
+            Sheet inputSheet = inputWorkbook.getSheetAt(0);
+            Sheet outputSheet = outputWorkbook.createSheet(inputSheet.getSheetName());
+
+            for (Row inputRow : inputSheet) {
+                Row outputRow = outputSheet.createRow(inputRow.getRowNum());
+
+                int lastCellIndex = inputRow.getLastCellNum();
+
+                for (int i = 0; i < lastCellIndex; i++) {
+                    Cell inputCell = inputRow.getCell(i);
+                    Cell outputCell = outputRow.createCell(i);
+
+                    if (inputCell != null) {
+                        copiazaCelula(inputCell, outputCell);
+                    }
+                }
+
+                Cell mediaCell = outputRow.createCell(lastCellIndex);
+
+                if (inputRow.getRowNum() == 0) {
+                    mediaCell.setCellValue("Media");
+                } else {
+                    double suma = 0;
+                    int count = 0;
+
+                    for (int i = lastCellIndex - 3; i < lastCellIndex; i++) {
+                        Cell cell = inputRow.getCell(i);
+
+                        if (cell != null && cell.getCellType() == CellType.NUMERIC) {
+                            suma += cell.getNumericCellValue();
+                            count++;
+                        }
+                    }
+
+                    if (count > 0) {
+                        mediaCell.setCellValue(suma / count);
+                    } else {
+                        mediaCell.setCellValue(0);
+                    }
+                }
+            }
+
+            if (inputSheet.getRow(0) != null) {
+                int nrColoane = inputSheet.getRow(0).getLastCellNum() + 1;
+
+                for (int i = 0; i < nrColoane; i++) {
+                    outputSheet.autoSizeColumn(i);
+                }
+            }
+
+            try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
+                outputWorkbook.write(outputStream);
+            }
+
+            System.out.println("Fisierul " + outputFile + " a fost creat.");
+
+        } catch (IOException e) {
+            System.out.println("Eroare la generarea fisierului " + outputFile);
+            e.printStackTrace();
+        }
+    }
+
+    private static void afiseazaCelula(Cell cell) {
+        switch (cell.getCellType()) {
+            case STRING:
+                System.out.print(cell.getStringCellValue());
+                break;
+
+            case NUMERIC:
+                System.out.print(cell.getNumericCellValue());
+                break;
+
+            case BOOLEAN:
+                System.out.print(cell.getBooleanCellValue());
+                break;
+
+            case FORMULA:
+                System.out.print(cell.getCellFormula());
+                break;
+
+            default:
+                System.out.print(" ");
+                break;
+        }
+    }
+
+    private static void copiazaCelula(Cell inputCell, Cell outputCell) {
+        switch (inputCell.getCellType()) {
+            case STRING:
+                outputCell.setCellValue(inputCell.getStringCellValue());
+                break;
+
+            case NUMERIC:
+                outputCell.setCellValue(inputCell.getNumericCellValue());
+                break;
+
+            case BOOLEAN:
+                outputCell.setCellValue(inputCell.getBooleanCellValue());
+                break;
+
+            case FORMULA:
+                outputCell.setCellFormula(inputCell.getCellFormula());
+                break;
+
+            default:
+                outputCell.setBlank();
+                break;
         }
     }
 }
